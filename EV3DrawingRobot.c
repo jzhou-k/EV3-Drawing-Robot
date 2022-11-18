07720b20e9c7b7115deee6b3f2ad121675dec0cd
341113090 <nathanthian@outlook.com>
2022-11-18 14:13:56 -0500

    RETURN TO BEGINING X AXIS

--- EV3DrawingRobot.c
+++ EV3DrawingRobot.c
@@ -1,9 +1,23 @@
-// empty robotc file
+/*
+    ASSUME: TOUCH SENSOR IS S2
+            MOTOR A IS THE X AXIS
 
+    This function will move the x axis motor to the begining (far left)
+    of the printer, where the pen is at the first pixel position.
+    The motor will reverse until it hits the touch sensor. 
+*/
+void returnToXHome() 
+{
+	  while(SensorValue[S2] != 1)
+	  {
+	      motor[motorA] = -50;
+	  }
+	  motor[motorA] = 0;
+	  nMotorEncoder[motorA] = 0;   
+}
 
 task main()
 {
-
-
-
+    sensorType[S2] = sensorEV3_Touch;
+    returnToHome(true);
 }
