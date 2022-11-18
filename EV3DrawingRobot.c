8aeead3190809d495e590ba94a8d3f4d6286e3da
jzhou-k <jiayizhou22918@gmail.com>
2022-11-18 13:43:55 -0500

    Update EV3DrawingRobot.c

--- EV3DrawingRobot.c
+++ EV3DrawingRobot.c
@@ -1,9 +1,26 @@
-// empty robotc file
-
+// not so empty robotc file 
+const double DPI = (1.0/16.0);
+const double INCH_TO_CM = 2.54; 
 
 task main()
 {
 
 
 
+}
+
+//enters unit num 
+int distYToPoint(int currY, int nextY) 
+{
+		return nextY - currY; 
+}
+
+double convertUnitToCM(int unit)
+{
+	return convertInchToCM(unit * DPI);
+}
+
+double convertInchToCM(double inches)
+{
+	return inches * INCH_TO_CM; 	
 }
