47b07fbce29af18c215d6a303ea9917da429b607
341113090 <nathanthian@outlook.com>
2022-11-24 18:35:02 -0500

    Update ports.h

--- menuSys/ports.h
+++ menuSys/ports.h
@@ -4,7 +4,7 @@ typedef enum ports
 	xAxis = motorA, // moves the dottor left and right
 	yAxis = motorB, // moves the paper
 	zAxis = motorC, // moves the dottor up and down
-	xAxisLim = S1, // touch sensor on x axis
-	zAxisLim = S2, // touch sensor on z axis (dotter)
-	paperSensor = S3 // color sensor for the paper on y axis
+	xAxisLim = S2, // touch sensor on x axis
+	zAxisLim = S3, // touch sensor on z axis (dotter)
+	paperSensor = S1 // color sensor for the paper on y axis
 };
